e7246e541a86033203075a5036172edaf9ea91d4
limitless <limitless@infinity.tx.rr.com>
2021-02-05 18:32:47 -0600

    beach drawing

--- public/sketch.js
+++ public/sketch.js
@@ -1,22 +1,25 @@
 let yoff = 0.0;
 
 function setup(){
-  const canvas = createCanvas(1000, 600)
-  // canvas.class("lemon")
-}
-
-function draw() {
+  const canvas =  createCanvas(windowWidth, windowHeight);
   background(233, 205, 164)
   noFill()
   stroke(255)
+  t = 0
+}
 
+function draw() {
+  fill(color(50, 30, 200,1))
   beginShape()
-  for (let x = 0; x <= width; x++) {
-    var nx = map(x, 0, width, 0, 10)
-    var y = height * noise(nx)
-
+  var xoff = 0
+  for (var x = 0; x <= width; x += 5) {
+    var y = map(noise(xoff, yoff), 0, 1, 100,300)
     vertex(x, y)
+    xoff += 0.02
   }
-  endShape()
+  yoff += 0.0085
+  vertex(width, height)
+  vertex(0, height)
+  endShape(CLOSE)
 }
 
